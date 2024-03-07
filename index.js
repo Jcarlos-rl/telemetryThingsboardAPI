40796b9ef3c23ff37cf967de6d57bf43db5e4bef
Jcarlos-rl <carlos_-619@hotmail.com>
2024-03-07 17:10:07 -0600

    backdrop

--- index.js
+++ index.js
@@ -7,19 +7,24 @@ const btn_request = document.querySelector('#btn_request'),
     startDay = document.querySelector('#startDay'),
     endDay = document.querySelector('#endDay'),
     content_feedback = document.querySelector('#content_feedback'),
-    btn_download = document.querySelector('#btn_download')
+    btn_download = document.querySelector('#btn_download'),
+    backdrop = document.querySelector('#backdrop')
     ;
 
 let newData = {}, endDayMil = 0;
 btn_request.addEventListener('click', ()=>{
+    backdrop.classList.remove('hidden');
     const startDayMil = convertDateToMilliseconds(new Date(startDay.value));
     endDayMil = (endDayMil === 0) ? convertDateToMilliseconds(new Date(endDay.value)) : endDayMil;
     let url = `${ urlServer.value }/api/plugins/telemetry/${ entityType.value }/${ entityId.value }/values/timeseries?keys=${ keys.value }&startTs=${ startDayMil }&endTs=${ endDayMil }&limit=1000`;
     fetchData(token.value, url).then(data=>{
+        backdrop.classList.add('hidden');
         if(Object.keys(data).length === 0){
             let startDayMilLocale = new Date(endDayMil), 
-                endDayMilLocale = new Date(convertDateToMilliseconds(new Date(endDay.value)));
-            content_feedback.innerHTML += `<p class="mb-4 text-green-600">Se obtubieron un total de ${ Object.keys(newData).length } registros en el rango de ${ startDayMilLocale.toLocaleString() } al ${ endDayMilLocale.toLocaleString() }.</p>`;
+                endDayMilLocale = new Date(convertDateToMilliseconds(new Date(endDay.value))),
+                prevContent = content_feedback.innerHTML;
+            content_feedback.innerHTML = `<p class="mb-4 text-green-600">Se obtubieron un total de ${ Object.keys(newData).length } registros en el rango de ${ startDayMilLocale.toLocaleString() } al ${ endDayMilLocale.toLocaleString() }.</p>`;
+            content_feedback.innerHTML += prevContent;
         }else{
             let countNewData = Object.keys(newData);
             let endTs = [];
@@ -43,8 +48,10 @@ btn_request.addEventListener('click', ()=>{
             let prevEndDayMil = endDayMil;
             endDayMil = (endTs.length>1) ? (Math.min(endTs)) : endTs[0];
             let endDayMilLocale = new Date(endDayMil),
-                prevEndDayMilLocale = new Date(prevEndDayMil);
-            content_feedback.innerHTML += `<p class="mb-4">Se obtubieron ${ Object.keys(newData).length - countNewData.length } registros en un rango de fechas de ${ endDayMilLocale.toLocaleString() } al ${ prevEndDayMilLocale.toLocaleString() }. Llevando un total de ${ Object.keys(newData).length } registros.</p>`;
+                prevEndDayMilLocale = new Date(prevEndDayMil),
+                prevContent = content_feedback.innerHTML;
+            content_feedback.innerHTML = `<p class="mb-4">Se obtubieron ${ Object.keys(newData).length - countNewData.length } registros en un rango de fechas de ${ endDayMilLocale.toLocaleString() } al ${ prevEndDayMilLocale.toLocaleString() }. Llevando un total de ${ Object.keys(newData).length } registros.</p>`;
+            content_feedback.innerHTML += prevContent;
         }
     });
 });
